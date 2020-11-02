5f47b9d757f3b41be5b6448b283caa8b433c74a7
ccor <ccor2005@gmail.com>
2020-11-02 15:40:20 +0800

    + mybatis-plus

---
+++ test-mybatis-plus/src/main/java/x/xx/data/entity/UserEntity.java
@@ -0,0 +1,78 @@
+package x.xx.data.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import java.time.LocalDateTime;
+import java.io.Serializable;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * <p>
+ * 用户
+ * </p>
+ *
+ * @author mybatis-plus
+ * @since 2020-11-02
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@TableName("t_user")
+public class UserEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 主键
+     */
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    /**
+     * 用户名
+     */
+    private String userName;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 昵称
+     */
+    private String nickName;
+
+    /**
+     * 姓名
+     */
+    private String realName;
+
+    /**
+     * 年龄
+     */
+    private Integer age;
+
+    /**
+     * 性别
+     */
+    private Integer sex;
+
+    /**
+     * 是否删除
+     */
+    private Integer isDeleted;
+
+    /**
+     * 创建时间
+     */
+    private LocalDateTime createdTime;
+
+    /**
+     * 更新时间
+     */
+    private LocalDateTime updatedTime;
+
+
+}
---
+++ test-mybatis-plus/src/main/java/x/xx/data/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package x.xx.data.mapper;
+
+import x.xx.data.entity.UserEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 用户 Mapper 接口
+ * </p>
+ *
+ * @author mybatis-plus
+ * @since 2020-11-02
+ */
+public interface UserMapper extends BaseMapper<UserEntity> {
+
+}
